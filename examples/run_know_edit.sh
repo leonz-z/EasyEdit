41fa4d7512e29b4e1363193e050108855d1167c2
li_zhixi <li_y_c@qq.com>
2024-06-03 16:43:52 +0800

    modify relative path 2024-6-3 16:46:58

--- examples/run_know_edit.sh
+++ examples/run_know_edit.sh
@@ -3,12 +3,12 @@
 DATE=2024-6-3
 
 # Ensure log directory exists
-if [ ! -d "log" ]; then
-    mkdir log
-fi
+# if [ ! -d "examples/log" ]; then
+#     mkdir examples/log
+# fi
 # if log/$DATE does not exist, create it
-if [ ! -d "log/$DATE" ]; then
-    mkdir "log/$DATE"
+if [ ! -d "examples/log/$DATE" ]; then
+    mkdir "examples/log/$DATE"
 fi
 
 # 3090
@@ -21,8 +21,9 @@ export HUGGINGFACE_CACHE=/home/bingxing2/public/models/llama2 # A100
 module load compilers/cuda/11.8
 module load cudnn/8.8.1.3_cuda11.x
 module load compilers/gcc/12.2.0
-source ~/.bashrc
-conda activate ke
+# source ~/.bashrc
+# conda activate ke
+source activate ke
 # DATA_TEST=ZsRE-test-all
 # DATA_TYPE=zsre
 EDIT_METHOD=ROME
@@ -31,10 +32,10 @@ DATA_TRAIN=recent_train
 DATA_TYPE=recent
 NUM=1
 
-CUDA_VISIBLE_DEVICES=0 nohup python run_knowedit_llama2.py \
+CUDA_VISIBLE_DEVICES=0 nohup python examples/run_knowedit_llama2.py \
     --editing_method $EDIT_METHOD \
-    --hparams_dir ../hparams/$EDIT_METHOD/$MODEL \
-    --data_dir ../LLMKnowledgeEditDataset/ccks2024_know_edit/$DATA_TEST.json \
-    --train_data_path ../LLMKnowledgeEditDataset/ccks2024_know_edit/$DATA_TRAIN.json \
+    --hparams_dir hparams/$EDIT_METHOD/$MODEL \
+    --data_dir LLMKnowledgeEditDataset/ccks2024_know_edit/$DATA_TEST.json \
+    --train_data_path LLMKnowledgeEditDataset/ccks2024_know_edit/$DATA_TRAIN.json \
     --datatype $DATA_TYPE \
-    > log/$DATE/$EDIT_METHOD-$MODEL-$DATA_TEST-$NUM.log 2>&1 &
+    > examples/log/$DATE/$EDIT_METHOD-$MODEL-$DATA_TEST-$NUM.log 2>&1 &
