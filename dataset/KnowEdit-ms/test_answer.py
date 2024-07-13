a41d01308ab1e9f126547c2c6e4aec640699a5a7
li_zhixi <li_y_c@qq.com>
2024-07-13 15:58:42 +0800

    update test_answer.py ipynb sh

--- dataset/KnowEdit-ms/test_answer.py
+++ dataset/KnowEdit-ms/test_answer.py
@@ -1,12 +1,12 @@
 import transformers
 import torch
 import json
-# import os
+import os
 # os.environ['CUDA_VISIBLE_DEVICES']= '0'
 model_name = 'Meta-Llama-3-8B-Instruct'
 # model_name = 'gpt-j-6b'
-# model_id = f"/share/huggingface/{model_name}"
-model_id = f"/home/bingxing2/public/models/llama3/{model_name}"
+huggingface_cache = os.environ.get('HUGGINGFACE_CACHE')
+model_id =huggingface_cache+model_name
 
 pipeline = transformers.pipeline("text-generation", 
                                  model=model_id, 
@@ -20,18 +20,21 @@ with open(data_path, 'r') as f:
     data_list = json.load(f)
     
 dataset_type = 'counterfact'
-with open(f'answer-{dataset_type}-{model_name}-n-words.json', 'a') as f:
-    for data in data_list[:100]:
+with open(f'answer-{dataset_type}-{model_name}-<=n+1-words.json', 'a') as f:
+    for data in data_list[:500]:
         prompt1 = data['prompt']
-        answer_len = len(data['ground_truth'])
-        prompt2 = f"Please response the input in {answer_len} words!\nInput:{data['prompt']}\nResponse:"
-        prompt3 = f"Please answer the question in {answer_len} words!\nQuestion:{data['prompt']}\nAnswer:"
+        answer_len = len(data['ground_truth'].split(' '))+1
+        # prompt2 = f"Please response the input in {answer_len} words!\nInput:{data['prompt']}\nResponse:"
+        # prompt3 = f"Please answer the question in {answer_len} words!\nQuestion:{data['prompt']}\nAnswer:"
+        prompt2 = f"Please response the input in no more than {answer_len} words!\nInput:{data['prompt']}\nResponse:"
+        prompt3 = f"Please answer the question in no more than {answer_len} words!\nQuestion:{data['prompt']}\nAnswer:"
         answer1 = pipeline(prompt1, max_new_tokens=answer_len, do_sample=False)
         answer2 = pipeline(prompt2, max_new_tokens=answer_len, do_sample=False)
         answer3 = pipeline(prompt3, max_new_tokens=answer_len, do_sample=False)
         data_dict = {
             'prompt': data['prompt'],
             'ground_truth': data['ground_truth'],
+            'answer_len': answer_len,
             'answer1': answer1[0]['generated_text'].replace(prompt1, ''),
             'answer2': answer2[0]['generated_text'].replace(prompt2, ''),
             'answer3': answer3[0]['generated_text'].replace(prompt3, ''),
--- dataset/KnowEdit-ms/test_answer.sh
+++ dataset/KnowEdit-ms/test_answer.sh
@@ -1,6 +1,11 @@
-module load compilers/cuda/12.1
-module load cudnn/8.9.5.29_cuda12.x
-module load compilers/gcc/12.2.0
+# module load compilers/cuda/12.1
+# module load cudnn/8.9.5.29_cuda12.x
+# module load compilers/gcc/12.2.0
+# source activate ke2torch23cu121
+# export HUGGINGFACE_CACHE=/home/bingxing2/home/scx7avs/lyc/huggingface/
+export CUDA_VISIBLE_DEVICES=0
+
 source activate ke2torch23cu121
+export HUGGINGFACE_CACHE=/share/huggingface/
 
 python test_answer.py
