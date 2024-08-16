00869213f5a55c2273bd191de2bceeb8e4817cc5
li_zhixi <li_y_c@qq.com>
2024-08-16 22:36:29 +0800

    0-3 knb batch

--- examples/run_3090/Qwen-1_8B-Chat.sh
+++ examples/run_3090/Qwen-1_8B-Chat.sh
@@ -7,15 +7,17 @@ export PYTHONUNBUFFERED=1
 
 # KNB
 method=KNB
-batch_size=1
-num_steps=80
+num_steps=60
 start_idx_end_idx=0,80
 data_type=type2_80
-p=99.5
+type=max 
+# type=mean
 cnt=1
 
-i=2
-echo $i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-Qwen-1_8B-Chat-$cnt
+batch_size=10
+p=99.05
+i=0
+echo $i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-$type-Qwen-1_8B-Chat-$cnt
 CUDA_VISIBLE_DEVICES=$i python examples/run_CKnowEdit_qwen-1.8B.py \
     --batch_size $batch_size \
     --num_steps $num_steps \
@@ -25,9 +27,60 @@ CUDA_VISIBLE_DEVICES=$i python examples/run_CKnowEdit_qwen-1.8B.py \
     --metrics_save_dir ./ccks2024_output/$data_type \
     --data_dir ./dataset/ccks2024_know_edit/CKnowEditType/$data_type.json \
     --start_idx_end_idx $start_idx_end_idx \
-    --knb_dict_path ../knowledge-neurons-lyc/tests/Qwen-1_8B-Chat_CKnowEdit_answer_next_token/type2_80-Qwen-1_8B-Chat-CKnowEdit-layer-0-24-mlp.w1-target_new-no-prompt-p_data_weight_layer_knb_dict.json \
+    --knb_dict_path ../knb_dict/Qwen-1_8B-Chat-CKnowEdit/$data_type-Qwen-1_8B-Chat-CKnowEdit-0-24-mlp.c_proj-knb_dict-$type-bs$batch_size.json \
+    --is_post_metrics \
     > logs/$DATE/$i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-Qwen-1_8B-Chat-$cnt.log 2>&1 &
-    # --is_post_metrics \
+
+batch_size=10
+p=99.95
+i=1
+echo $i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-$type-Qwen-1_8B-Chat-$cnt
+CUDA_VISIBLE_DEVICES=$i python examples/run_CKnowEdit_qwen-1.8B.py \
+    --batch_size $batch_size \
+    --num_steps $num_steps \
+    --editing_method $method \
+    --p $p \
+    --data_type CKnowEdit_$data_type \
+    --metrics_save_dir ./ccks2024_output/$data_type \
+    --data_dir ./dataset/ccks2024_know_edit/CKnowEditType/$data_type.json \
+    --start_idx_end_idx $start_idx_end_idx \
+    --knb_dict_path ../knb_dict/Qwen-1_8B-Chat-CKnowEdit/$data_type-Qwen-1_8B-Chat-CKnowEdit-0-24-mlp.c_proj-knb_dict-$type-bs$batch_size.json \
+    --is_post_metrics \
+    > logs/$DATE/$i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-Qwen-1_8B-Chat-$cnt.log 2>&1 &
+
+batch_size=20
+p=99.05
+i=2
+echo $i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-$type-Qwen-1_8B-Chat-$cnt
+CUDA_VISIBLE_DEVICES=$i python examples/run_CKnowEdit_qwen-1.8B.py \
+    --batch_size $batch_size \
+    --num_steps $num_steps \
+    --editing_method $method \
+    --p $p \
+    --data_type CKnowEdit_$data_type \
+    --metrics_save_dir ./ccks2024_output/$data_type \
+    --data_dir ./dataset/ccks2024_know_edit/CKnowEditType/$data_type.json \
+    --start_idx_end_idx $start_idx_end_idx \
+    --knb_dict_path ../knb_dict/Qwen-1_8B-Chat-CKnowEdit/$data_type-Qwen-1_8B-Chat-CKnowEdit-0-24-mlp.c_proj-knb_dict-$type-bs$batch_size.json \
+    --is_post_metrics \
+    > logs/$DATE/$i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-Qwen-1_8B-Chat-$cnt.log 2>&1 &
+
+batch_size=20
+p=99.95
+i=3
+echo $i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-$type-Qwen-1_8B-Chat-$cnt
+CUDA_VISIBLE_DEVICES=$i python examples/run_CKnowEdit_qwen-1.8B.py \
+    --batch_size $batch_size \
+    --num_steps $num_steps \
+    --editing_method $method \
+    --p $p \
+    --data_type CKnowEdit_$data_type \
+    --metrics_save_dir ./ccks2024_output/$data_type \
+    --data_dir ./dataset/ccks2024_know_edit/CKnowEditType/$data_type.json \
+    --start_idx_end_idx $start_idx_end_idx \
+    --knb_dict_path ../knb_dict/Qwen-1_8B-Chat-CKnowEdit/$data_type-Qwen-1_8B-Chat-CKnowEdit-0-24-mlp.c_proj-knb_dict-$type-bs$batch_size.json \
+    --is_post_metrics \
+    > logs/$DATE/$i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-Qwen-1_8B-Chat-$cnt.log 2>&1 &
 
 
 # # LoRA
