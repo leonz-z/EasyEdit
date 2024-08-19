e60e3b0e96d3d57c41bc0ac10e9ac8aaa54d6d33
li_zhixi <li_y_c@qq.com>
2024-08-19 16:43:16 +0800

    update qwen1.8b.chat.sh

--- examples/run_3090/Qwen-1_8B-Chat.sh
+++ examples/run_3090/Qwen-1_8B-Chat.sh
@@ -5,53 +5,34 @@ export HUGGINGFACE_CACHE=/share/huggingface/
 export PYTHONUNBUFFERED=1
 # export CUDA_VISIBLE_DEVICES=1
 
-# KNB
-method=KNB
-# 当batch_size=1时,num_steps设置小一点可能效果好,防止过拟合
-num_steps=40
-batch_size=1
-type=orgin 
+# MEMIT
+method=MEMIT
+start_idx_end_idx=0,133
+data_type=type1_133
+max_new_tokens_times=2
 
-# num_steps=120
-# batch_size=20
-# type=max 
-# type=mean
-start_idx_end_idx=0,80
-data_type=type2_80
+layers=0,12
 cnt=1
-
-
-# p=95.0
-# 当batch_size=1时,p设置小一点可能效果好
-p=99.95 
-i=2
-echo $i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-$type-Qwen-1_8B-Chat-$cnt
-CUDA_VISIBLE_DEVICES=$i python examples/run_CKnowEdit_qwen-1.8B.py \
-    --batch_size $batch_size \
-    --num_steps $num_steps \
+gpu=0
+CUDA_VISIBLE_DEVICES=$gpu python examples/run_CKnowEdit_qwen-1.8B.py \
     --editing_method $method \
-    --p $p \
+    --layers $layers \
+    --start_idx_end_idx $start_idx_end_idx \
     --data_type CKnowEdit_$data_type \
     --metrics_save_dir ./ccks2024_output/$data_type \
     --data_dir /share/dataset/CKnowEditType/$data_type.json \
-    --start_idx_end_idx $start_idx_end_idx \
-    --knb_dict_path /share/knb_dict/Qwen-1_8B-Chat-CKnowEdit/$data_type-Qwen-1_8B-Chat-CKnowEdit-0-24-mlp.c_proj-knb_dict-$type-bs$batch_size.json \
-    --is_post_metrics \
-    > logs/$DATE/$i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-Qwen-1_8B-Chat-$cnt.log 2>&1 &
+    --max_new_tokens_times $max_new_tokens_times \
+    > logs/$DATE/$gpu-$data_type-tt$max_new_tokens_times-$method-$layers-$start_idx_end_idx-Qwen-1_8B-Chat-$cnt.log 2>&1 &
 
-# p=90.0
-p=99.05
-i=3
-echo $i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-$type-Qwen-1_8B-Chat-$cnt
-CUDA_VISIBLE_DEVICES=$i python examples/run_CKnowEdit_qwen-1.8B.py \
-    --batch_size $batch_size \
-    --num_steps $num_steps \
+layers=12,24
+cnt=1
+gpu=3
+CUDA_VISIBLE_DEVICES=$gpu python examples/run_CKnowEdit_qwen-1.8B.py \
     --editing_method $method \
-    --p $p \
+    --layers $layers \
+    --start_idx_end_idx $start_idx_end_idx \
     --data_type CKnowEdit_$data_type \
     --metrics_save_dir ./ccks2024_output/$data_type \
-    --data_dir ./dataset/ccks2024_know_edit/CKnowEditType/$data_type.json \
-    --start_idx_end_idx $start_idx_end_idx \
-    --knb_dict_path ../knb_dict/Qwen-1_8B-Chat-CKnowEdit/$data_type-Qwen-1_8B-Chat-CKnowEdit-0-24-mlp.c_proj-knb_dict-$type-bs$batch_size.json \
-    --is_post_metrics \
-    > logs/$DATE/$i-$p-$start_idx_end_idx-$method-$batch_size-$num_steps-$data_type-Qwen-1_8B-Chat-$cnt.log 2>&1 &
+    --data_dir /share/dataset/CKnowEditType/$data_type.json \
+    --max_new_tokens_times $max_new_tokens_times \
+    > logs/$DATE/$gpu-$data_type-tt$max_new_tokens_times-$method-$layers-$start_idx_end_idx-Qwen-1_8B-Chat-$cnt.log 2>&1 &
