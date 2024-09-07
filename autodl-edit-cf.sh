bfc93047bc0ef29b7b4e851ac9d09737d61ac751
li_zhixi <li_y_c@qq.com>
2024-09-07 19:39:14 +0800

    stage edit-cf.sh

--- autodl-edit-cf.sh
+++ autodl-edit-cf.sh
@@ -45,8 +45,8 @@ gpus=5
 cnt=1
 batch=$((885/$gpus))
 
-# type=mean 
-type=max
+type=mean 
+# type=max
 # t_loss
 # for t_loss in 1e-1 2e-1 3e-1 4e-1 5e-1 6e-1 7e-1 8e-1 9e-1 5e-2 1e-2 5e-3 1e-3; do
 # p
