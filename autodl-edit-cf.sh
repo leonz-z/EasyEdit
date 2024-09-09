6bf5317fc6df8392b61b5388bbebeabb1edfa111
li_zhixi <li_y_c@qq.com>
2024-09-09 19:35:13 +0800

    audl-edit-cf.sh

--- autodl-edit-cf.sh
+++ autodl-edit-cf.sh
@@ -5,10 +5,12 @@ export HUGGINGFACE_CACHE=/root/autodl-fs/
 export PYTHONUNBUFFERED=1
 # export CUDA_VISIBLE_DEVICES=1
 
-# # model
-model=gpt-j-6b
-ff_attrs=mlp.fc_out
+# model = 'gpt-j-6b'
+# weight_name_list = ['attn.k_proj', 'attn.v_proj', 'attn.q_proj', 'attn.out_proj', 'mlp.fc_in', 'mlp.fc_out']
+# model = 'llama2-7b'
+# weight_name_list = ['self_attn.q_proj', 'self_attn.k_proj', 'self_attn.v_proj', 'self_attn.o_proj', 'mlp.up_proj', 'mlp.gate_proj', 'mlp.down_proj']
 
+# model
 # model=llama3-8b
 # model=llama2-7b
 # model=qwen1.5-7b
@@ -22,11 +24,9 @@ ff_attrs=mlp.fc_out
 
 # KNB
 method=KNB
-knb_layer=last_layer
-# type=max 
-# p=99.9
+knb_layer=this_layer # last_layer # this_layer
+type=max # mean
 t_loss=0.7
-batch_size=30
 num_steps=100
 
 # dataset 
@@ -45,15 +45,18 @@ gpus=5
 cnt=1
 batch=$((885/$gpus))
 
-type=mean 
-# type=max
 # t_loss
 # for t_loss in 1e-1 2e-1 3e-1 4e-1 5e-1 6e-1 7e-1 8e-1 9e-1 5e-2 1e-2 5e-3 1e-3; do
 # p
-for p in 90 93 96 99 99.3 99.6 99.9; do
+# for p in 90 93 96 99 99.3 99.6 99.9; do
+model=gpt-j-6b
+ff_attrs=mlp.fc_out
+p=99
+for batch_size in 1 2 4 6 8 10; do
     for gpu in $(seq 0 $(($gpus-1))); do
         start_idx_end_idx=$(($gpu*$batch)),$(($gpu*$batch+$batch))
-        knb_dict_path=/root/autodl-fs/knb-dict-2024/$model/$data_type/${next_token}_target_new/$ff_attrs/bs$batch_size-p$p-$type.json
+        # knb_dict_path=/root/autodl-fs/knb-dict-2024/$model/$data_type/${next_token}_target_new/$ff_attrs/bs$batch_size-p$p-$type.json
+        knb_dict_path=/root/autodl-fs/knb-dict-2024/$model/$data_type/$knb_layer/bs$batch_size-p$p-$type.json
         echo $gpu-$p-$ff_attrs-$t_loss-$start_idx_end_idx-$method-bs$batch_size-epoch$num_steps-$data_type-$type-$model-$knb_layer-$cnt
         CUDA_VISIBLE_DEVICES=$gpu python examples/run_knowedit.py \
             --knb_layer $knb_layer \
@@ -73,3 +76,31 @@ for p in 90 93 96 99 99.3 99.6 99.9; do
     done
     wait
 done
+wait
+
+# model=llama2-7b
+# ff_attrs=mlp.down_proj
+# p=93
+# for batch_size in 1 2 4 6 8 10; do
+#     for gpu in $(seq 0 $(($gpus-1))); do
+#         start_idx_end_idx=$(($gpu*$batch)),$(($gpu*$batch+$batch))
+#         knb_dict_path=/root/autodl-fs/knb-dict-2024/$model/$data_type/${next_token}_target_new/$ff_attrs/bs$batch_size-p$p-$type.json
+#         echo $gpu-$p-$ff_attrs-$t_loss-$start_idx_end_idx-$method-bs$batch_size-epoch$num_steps-$data_type-$type-$model-$knb_layer-$cnt
+#         CUDA_VISIBLE_DEVICES=$gpu python examples/run_knowedit.py \
+#             --knb_layer $knb_layer \
+#             --target_modules $ff_attrs \
+#             --t_loss $t_loss \
+#             --batch_size $batch_size \
+#             --num_steps $num_steps \
+#             --editing_method $method \
+#             --p $p \
+#             --data_type $data_type \
+#             --data_dir $data_dir \
+#             --start_idx_end_idx $start_idx_end_idx \
+#             --knb_dict_path $knb_dict_path \
+#             --hparams_dir ./hparams/KNB/$model.yaml \
+#             --pre_file ./pre_edit/${model}_${data_type}_pre_edit.json \
+#             > logs/$DATE/$gpu-$p-$ff_attrs-$t_loss-$start_idx_end_idx-$method-bs$batch_size-epoch$num_steps-$data_type-$type-$model-$knb_layer-$cnt.log 2>&1 &
+#     done
+#     wait
+# done
