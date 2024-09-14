29fe9f0ac62c3a6f1a4b935e01da205b641336d1
li_zhixi <li_y_c@qq.com>
2024-09-14 08:47:40 +0800

    autodl-edit-llama-mlp.sh

--- autodl-edit-llama-mlp.sh
+++ autodl-edit-llama-mlp.sh
@@ -15,10 +15,10 @@ export PYTHONUNBUFFERED=1
 # KNB
 method=KNB
 knb_layer=this_layer # last_layer，this_layer
-type=max # mean
+type=max             # mean
 num_steps=100
 
-# dataset 
+# dataset
 # zsre数据部分字段有缺失
 data_dir=./dataset/KnowEdit-ms/benchmark_wiki_counterfact_test_cf.json
 data_type=counterfact
@@ -27,40 +27,37 @@ data_type=counterfact
 # 5张gpu
 gpus=5
 cnt=1
-batch=$((885/$gpus))
+batch=$((885 / $gpus))
 
 model=llama2-7b-chat
-batch_size=10 
+batch_size=20
 t_loss=0.4
 start_idx_end_idx=0,885
 # for p in 90 92 94 96 98; do
-p=90
-for n in 200 400 600 800; do
-  for ff_attrs in mlp.up_proj mlp.down_proj mlp.up_proj,mlp.gate_proj,mlp.down_proj; do
-    for gpu in $(seq 0 $(($gpus - 1))); do
-      start_idx_end_idx=$(($gpu * $batch)),$(($gpu * $batch + $batch))
-      knb_dict_path=/root/autodl-fs/knb-dict-bs/llama2-7b-chat/counterfact/this_layer/n$n-p$p-max.json
-      # echo $knb_dict_path
-      echo $gpu-$p-$n-$ff_attrs-$t_loss-$start_idx_end_idx-$method-bs$batch_size-epoch$num_steps-$data_type-$type-$model-$knb_layer-$cnt
-      CUDA_VISIBLE_DEVICES=$gpu python examples/run_knowedit.py \
-        --knb_layer $knb_layer \
-        --target_modules $ff_attrs \
-        --t_loss $t_loss \
-        --batch_size $batch_size \
-        --num_steps $num_steps \
-        --editing_method $method \
-        --p $p \
-        --data_type $data_type \
-        --data_dir $data_dir \
-        --start_idx_end_idx $start_idx_end_idx \
-        --knb_dict_path $knb_dict_path \
-        --hparams_dir ./hparams/KNB/$model.yaml \
-        --pre_file ./pre_edit/${model}_${data_type}_pre_edit.json \
-        > logs/$DATE/$gpu-$p-$n-$ff_attrs-$t_loss-$start_idx_end_idx-$method-bs$batch_size-epoch$num_steps-$data_type-$type-$model-$knb_layer-$cnt.log 2>&1 &
-    done
-    wait
+p=96
+# for n in 200 400 600 800; do
+n=200
+for ff_attrs in mlp.gate_proj; do
+  for gpu in $(seq 0 $(($gpus - 1))); do
+    start_idx_end_idx=$(($gpu * $batch)),$(($gpu * $batch + $batch))
+    knb_dict_path=/root/autodl-fs/knb-dict-bs/llama2-7b-chat/counterfact/this_layer/n$n-p$p-max.json
+    # echo $knb_dict_path
+    echo $gpu-$p-$n-$ff_attrs-$t_loss-$start_idx_end_idx-$method-bs$batch_size-epoch$num_steps-$data_type-$type-$model-$knb_layer-$cnt
+    CUDA_VISIBLE_DEVICES=$gpu python examples/run_knowedit.py \
+      --knb_layer $knb_layer \
+      --target_modules $ff_attrs \
+      --t_loss $t_loss \
+      --batch_size $batch_size \
+      --num_steps $num_steps \
+      --editing_method $method \
+      --p $p \
+      --data_type $data_type \
+      --data_dir $data_dir \
+      --start_idx_end_idx $start_idx_end_idx \
+      --knb_dict_path $knb_dict_path \
+      --hparams_dir ./hparams/KNB/$model.yaml \
+      --pre_file ./pre_edit/${model}_${data_type}_pre_edit.json \
+      >logs/$DATE/$gpu-$p-$n-$ff_attrs-$t_loss-$start_idx_end_idx-$method-bs$batch_size-epoch$num_steps-$data_type-$type-$model-$knb_layer-$cnt.log 2>&1 &
   done
+  wait
 done
-
-
-
